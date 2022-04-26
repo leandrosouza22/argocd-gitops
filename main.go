fdbce487e4d02e62fb7480c29a503fee4d7a9e0b
Leandro Ribeiro de Souza <leandro.ribeiro@hash.com.br>
2022-04-26 11:48:59 -0300

    gitleaks

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 
@@ -8,4 +11,5 @@ func main() {
 		w.Write([]byte("<h1>Hello DevSecOps!!!</h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
+	fmt.Println("a4db08b7-5729-4ba9-8c08-f2df493465a1")
 }
