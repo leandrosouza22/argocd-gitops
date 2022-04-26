be226474db78b5cce1429ed6dacaa6981d5234db
Leandro Ribeiro de Souza <leandro.ribeiro@hash.com.br>
2022-04-26 12:05:45 -0300

    gitleaks

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 		w.Write([]byte("<h1>Hello DevSecOps!!!</h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
-	fmt.Println("ec3ca5d1-02dc-4a61-ba2f-f7fc109683b0")
+	fmt.Println("key:ec3ca5d1-02dc-4a61-ba2f-f7fc109683b0")
 }
