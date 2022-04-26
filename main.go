6c0d44d977ab9d5fc0625deb9c101188a6962116
Leandro Ribeiro de Souza <leandro.ribeiro@hash.com.br>
2022-04-26 11:47:07 -0300

    gitleaks

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello DevSecOps 2!</h1>"))
+		w.Write([]byte("<h1>Hello DevSecOps!!!</h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
