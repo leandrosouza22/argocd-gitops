e6361f06071b2ec20e34ba80ed2cc217c8ff41ae
Leandro Ribeiro <97611161+leandrosouza22@users.noreply.github.com>
2022-05-19 11:49:19 -0300

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello DevSecOps!!!</h1>"))
+		w.Write([]byte("<h1>Hello DevSecOps!!!</h1>")) 
 	})
 	http.ListenAndServe(":8080", nil)
 }
